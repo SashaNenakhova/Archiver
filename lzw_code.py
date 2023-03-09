1a49ebdc509f46364fe2bc8b630400b95c09987a
SashaNenakhova <sasha.nenakhova@gmail.com>
2023-03-09 18:12:26 +0500

    def decode file
    def get_sequence()
    def max_index()

--- lzw_code.py
+++ lzw_code.py
@@ -38,34 +38,13 @@
 ############################################################
 
 
-
-
-
-
-
-# convert index of sequence to bin code
-# добавить нули в начало
-# (encoding)
-def get_index(dictionary, n):
-    # максимальное количество цифр в индексе
-    max_index=len(bin(len(dictionary)-1).replace("0b", ""))
-
-    # количество цифр в индексе
-    current_index=str(bin(dictionary.index(n)).replace("0b", ""))
-
-    # добавлять нули в начало, пока длина индекса не равна максимальному количеству цифр
-    while len(current_index)<max_index:
-        current_index='0'+current_index
-    return current_index
-
-
 # reading file
 # (encoding)
 def read_file(file_name):
     try:
         file=open(file_name, 'r')
         return file.read()
-    except: 
+    except:
         pass
 
 
@@ -79,6 +58,22 @@ def init_dict(symbols):
     return dictionary
 
 
+# get dictionary index of sequence
+# convert dictionary index of sequence to bin code
+# добавить нули в начало
+# (encoding)
+def get_index(dictionary, n):
+    # количество цифр в индексе последовательности n
+    current_index=str(bin(dictionary.index(n)).replace("0b", ""))
+    # current_index = str(bin(dictionary.index(n)))
+
+    # добавлять нули в начало, пока длина индекса не равна количеству цифр в самом большом индексе
+    while len(current_index)<len(bin(len(dictionary)-1).replace("0b", "")):
+    # while len(current_index) < len(bin(len(dictionary) - 1)):
+        current_index='0'+current_index
+    return current_index
+
+
 # find max length
 # (encoding)
 def max_length(dictionary):
@@ -90,30 +85,52 @@ def max_length(dictionary):
 
 
 
+# найти самый длинный код в словаре
+# (decoding)
+def max_index(dictionary):
+    return len(bin(len(dictionary)-1).replace("0b", ""))
+
+
+# get sequence from dictionary by index
+# (decoding)
+def get_sequence(dictionary, bin_ind):
+    # convert bin code to decimal index
+    # bin_ind=int(bin(int(bin_ind)), 2)
+    ind=int('0b'+bin_ind, 2)
+    print('get_sequence: ', ind)
+
+
+    return dictionary[ind]
+
+
+
+
 
 
 
 
 
 ### encoding sequence of symbols
-def encode_file(file):
+def encode(file):
     # создание начального словаря
     dictionary=init_dict(file)
+
     # зашифрованный файл
     code = ''
 
     # предыдущая последовательность
     last_seq=''
+
     # длина самой большой последовательности из словаря
     max_seq=max_length(dictionary)
 
     # steps
-    step=0
+    # step=0
 
     ### пока не закончится файл
     while file!='':
         # steps
-        print('step № '+str(step))
+        # print('step № '+str(step))
 
         # из файла берется последовательность символов,
         # длина которой равна длине самой большой последовательности в словаре
@@ -133,9 +150,10 @@ def encode_file(file):
         # добавляем к коду индекс последовательности
         code+=get_index(dictionary, current_seq)
 
-        print('current sequence: ' + current_seq)
-        print('code: '+code)
-        print('file: '+file)
+        # print
+        # print('current sequence: ' + current_seq)
+        # print('code: '+code)
+        # print('file: '+file)
         print('dictionary: '+str(dictionary))
 
         # обновляется предыдущая последовательность символов
@@ -144,8 +162,8 @@ def encode_file(file):
         file = file[max_seq + 1:-1]
 
         # steps
-        step+=1
-        print('\n')
+        # step+=1
+        # print('\n')
 
     return code
 
@@ -158,8 +176,64 @@ def encode_file(file):
 
 
 ### decoding
-def decode(code):
-    pass
+def decode(code, dictionary):
+    # расшифрованный файл
+    file=''
+
+    # предыдущая последовательность
+    last_seq = ''
+
+    # длина последнего кода в словаре
+    max_num=max_index(dictionary)
+
+    # steps
+    step=0
+
+    # пока не закончится зашифрованный файл
+    while code!='':
+        # steps
+        print('step № '+str(step))
+
+        # из зашифрованного файла берется часть кода,
+        # длина которой равна длине последнего кода в словаре
+        current_code=code[:max_num]
+        print('index: ' + str(current_code))
+
+        # get sequence from the dictionary by code
+        flag=False
+        while flag==False:
+            try:
+                current_seq=get_sequence(dictionary, current_code)
+                flag=True
+            except:
+                current_code = current_code[:-1]
+
+        # add new sequence to the dictionary
+        if (last_seq+current_seq[0]) not in dictionary:
+            dictionary.append(last_seq + current_seq[0])
+            # длина самой большой записи в словаре
+            max_num=max_index(dictionary)
+
+        # добавляем к расшифрованному файлу последовательность символов
+        file+=current_seq
+
+        # print
+        print('current sequence: ' + current_seq)
+        print('current index: ' + str(current_code))
+        print('code: '+code)
+        print('file: '+file)
+        print('dictionary: '+str(dictionary))
+
+        # обновляется предыдущая последовательность символов
+        last_seq = current_seq                                  ############      !!!!!!!!!!!! last seq
+        # из начала файла удаляются зашифрованные символы
+        code = code[max_num + 1:-1]
+
+        # steps
+        step+=1
+        print('\n')
+
+    return file
 
 
 
@@ -174,13 +248,22 @@ def decode(code):
 file=read_file('input_file')
 
 # кодирование файла
-code=encode_file(file)
+code=encode(file)
 
 print('ENCODED FILE: '+code+'\n')
 
+# расшифровка кода
+dictionary=init_dict(file)
+decoded_file=decode(code, dictionary)
+
+print('DECODED CODE: '+decoded_file+'\n')
 
 
-
+# 0000100010100000001001001000100110000100000111100001101111001100100001010
+# 000b000b10000b100b100000b0000b1000b1000b10000b10010b100000b1000000b10b11100000b1100b1111000b11000b100000b1010
+#
+# dictionary: ['d', 'a', 'e', 'c', 'b', 'de', 'ea', 'ae', 'ed', 'eb', 'be',
+#               'ede', 'ebe', 'edb', 'ba', 'ab', 'bac', 'ca', 'abe', 'eae']
 
 
 
