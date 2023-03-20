ed7e0b8debe759efdc0319359a1f78e56b8c455e
SashaNenakhova <sasha.nenakhova@gmail.com>
2023-03-20 17:14:18 +0500

    two files
    lzw_compress creates compressed file from input file
    
    lzw_uncompress creates uncompressed file (input file) from archive

---
+++ lzw_compress.py
@@ -0,0 +1,159 @@
+
+# reading file
+# (encoding)
+def read_file(file_name):
+    try:
+        file=open(file_name, 'r')
+        return file.read()
+    except:
+        pass
+
+
+# creating compressed file
+# writing initial dictionary and coded file
+# (encoding)
+def compress_file(code, initial_dictionary, file_name):
+    file=open(file_name+'_archive', 'w')
+    file.write(initial_dictionary+'\n')
+    file.write(code)
+
+
+# creating initial dict
+# (encoding)
+def init_dict(symbols):
+    dictionary=[]
+    for i in symbols:
+        if i not in dictionary:
+            dictionary.append(i)
+    return dictionary
+
+
+# get dictionary index of sequence
+# convert dictionary index of sequence to bin code
+# добавить нули в начало
+# (encoding)
+def get_index(dictionary, n):
+    # количество цифр в индексе последовательности n
+    current_index=str(bin(dictionary.index(n)).replace("0b", ""))
+    # current_index = str(bin(dictionary.index(n)))
+
+    # добавлять нули в начало, пока длина индекса не равна количеству цифр в самом большом индексе
+    while len(current_index)<len(bin(len(dictionary)-1).replace("0b", "")):
+    # while len(current_index) < len(bin(len(dictionary) - 1)):
+        current_index='0'+current_index
+    return current_index
+
+
+# find max length
+# (encoding)
+def max_length(dictionary):
+    l=0
+    for i in dictionary:
+        if len(i)>l:
+            l=len(i)
+    return l
+
+
+
+
+
+
+
+
+
+
+### encoding sequence of symbols
+def encode(file):
+    # создание начального словаря
+    dictionary=init_dict(file)
+
+    # сохранение начального словаря для записи в зашифрованный файл
+    initial_dictionary=''.join(init_dict(file))
+
+    # зашифрованный файл
+    code = ''
+
+    # предыдущая последовательность
+    last_seq=''
+
+    # длина самой большой последовательности из словаря
+    max_seq=max_length(dictionary)
+
+    # steps
+    # step=0
+
+    ### пока не закончится файл
+    while file!='':
+        # steps
+        # print('step № '+str(step))
+
+        # из файла берется последовательность символов,
+        # длина которой равна длине самой большой последовательности в словаре
+        current_seq=file[:max_seq]
+        # print('sequence: '+current_seq)
+
+        # пока последовательности нет в словаре укорачивается на 1
+        while current_seq not in dictionary:
+            current_seq=current_seq[:-1]
+
+        # добавление новой последовательности (предыдущая+первый символ текущей)
+        if (last_seq+current_seq[0]) not in dictionary:
+            dictionary.append(last_seq + current_seq[0])
+            # длина самой большой записи в словаре
+            max_seq = max_length(dictionary)
+
+
+        # добавляем к коду индекс последовательности
+        code+=get_index(dictionary, current_seq)
+
+        # print
+        # print('current sequence: ' + current_seq)
+        # print('index: '+get_index(dictionary, current_seq))
+        # print('code: '+code)
+        # print('file: '+file)
+        # print('dictionary: '+str(dictionary))
+
+        # обновляется предыдущая последовательность символов
+        last_seq = current_seq
+        # из начала файла удаляются зашифрованные символы
+        file = file[len(current_seq):]
+
+        # steps
+        # step+=1
+        # print('\n')
+
+    return code, initial_dictionary
+
+
+
+
+
+
+
+
+
+
+
+
+
+### MAIN ###
+
+### прочитать файл, создать в зашифрованном файле начальный словарь в виде строки ('abcdef')
+
+# чтение файла
+file_name='input_file'
+file=read_file(file_name)
+
+print('FILE: '+file+'\n')
+
+
+
+# кодирование файла
+code, dict=encode(file)
+
+print('CODE: '+code+'\n')
+print('INIT DICTIONARY: '+dict+'\n')
+
+# change compressed_file
+# записать словарь и код в зашифрованный файл
+compress_file(code, dict, file_name)
---
+++ lzw_uncompress.py
@@ -0,0 +1,135 @@
+
+# reading file
+# (encoding)
+def read_file(file_name):
+    try:
+        file=open(file_name, 'r')
+        return file.read()
+
+    except:
+        pass
+
+# найти самый длинный код в словаре
+# (decoding)
+def max_index(dictionary):
+    return len(bin(len(dictionary)).replace("0b", ""))
+
+
+# get sequence from dictionary by index
+# (decoding)
+def get_sequence(dictionary, bin_ind):
+    # convert bin code to decimal index
+    # bin_ind=int(bin(int(bin_ind)), 2)
+    ind=int('0b'+bin_ind, 2)
+    # print('get_sequence: ', ind)
+
+
+    return dictionary[ind]
+
+
+# creating uncompressed file
+# (decoding)
+def uncompress_file(decoded_file, file_name):
+    file = open(file_name+'_uncompressed', 'w')
+    file.write(decoded_file)
+    file.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### decoding
+def decode(code, dictionary):
+    # расшифрованный файл
+    file=''
+
+    # предыдущая последовательность
+    last_seq = ''
+
+    # длина последнего кода в словаре
+    max_num=max_index(dictionary)
+
+    # steps
+    # step=0
+
+    # пока не закончится зашифрованный файл
+    while code!='':
+        # steps
+        # print('step № '+str(step))
+
+        # из зашифрованного файла берется часть кода,
+        # длина которой равна длине последнего кода в словаре
+        current_code=code[:max_num]
+        # print('index: ' + str(current_code))
+
+        # get sequence from the dictionary by code
+        flag=False
+        while flag==False:
+            try:
+                current_seq=get_sequence(dictionary, current_code)
+                flag=True
+            except:
+                current_code = current_code[:-1]
+
+        # add new sequence to the dictionary
+        if (last_seq+current_seq[0]) not in dictionary:
+            dictionary.append(last_seq + current_seq[0])
+            # длина самой большой записи в словаре
+            max_num=max_index(dictionary)
+
+        # добавляем к расшифрованному файлу последовательность символов
+        file+=current_seq
+
+        # print
+        # print('current sequence: ' + current_seq)
+        # print('current index: ' + str(current_code))
+        # print('code: '+code)
+        # print('file: '+file)
+        # print('dictionary: '+str(dictionary))
+
+        # обновляется предыдущая последовательность символов
+        last_seq = current_seq                                  ############      !!!!!!!!!!!! last seq
+        # из начала файла удаляются зашифрованные символы
+        # code = code[max_num + 1:-1]
+        code = code[len(str(current_code)):]
+
+        # steps
+        # step+=1
+        # print('\n')
+
+    return file
+
+
+
+
+
+
+### MAIN ###
+
+# чтение заархивированного файла
+file_name='input_file_archive'
+file=read_file(file_name).split('\n')
+
+# чтение кода и начального словаря из файла
+code=file[1]
+initial_dictionary=[]
+initial_dictionary.extend(file[0])
+
+
+# расшифровка файла
+decoded_file=decode(code, initial_dictionary)
+
+print('DECODED CODE: '+decoded_file+'\n')
+
+# создать расшифрованный файл
+uncompress_file(decoded_file, file_name)
+
